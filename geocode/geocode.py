e55478f901dd3fdc456250758c09114e77ab567e
Seth Flaxman <seth@flaxl-vb.(none)>
2012-04-03 11:50:07 -0400

    should be in a working state. usage: python geocode.py input.csv > output.csv

---
+++ geocode/geocode.py
@@ -0,0 +1,49 @@
+# assign census tracts and areas to 311 calls 
+
+import csv
+import pylab as pl
+import numpy as np
+import csv
+from shapely.geometry import Point, Polygon
+from census import tracts, tracts_to_area
+import sys
+
+if len(sys.argv) < 2:
+    print "usage: %s input.csv"%sys.argv[0]
+    sys.exit()
+
+data = csv.reader(open(sys.argv[1],"r"))
+
+def match_tract(lat,long,tracts=tracts):
+    for t in tracts.keys():
+        if tracts[t].contains(Point(long,lat)):
+            return t
+    else:
+        return 'did not match'
+
+cols = data.next()
+date_i = cols.index('date')
+lat_i = cols.index('lat')
+long_i = cols.index('long')
+cat_i = cols.index('type')
+
+print "date,type,area,tract"
+cats = []
+i = 0
+for row in data:
+    try:
+        date = row[date_i]
+        cat = row[cat_i]
+        lat = float(row[lat_i])
+        long = float(row[long_i])
+
+        if cat != '' and date != '':
+            try:
+                id = match_tract(lat,long)
+                tract, area = tracts_to_area[id]
+                print "%s,%s,%s,%s"%(date,cat.lower(),area,tract)
+            except:
+                #print "didn't match", row
+                pass
+    except:
+        pass
