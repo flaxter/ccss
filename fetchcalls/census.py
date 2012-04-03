b868b7efb41d17dce2be88c0fffa103269373d45
U-flaxl\seth <flaxman@gmail.com>
2012-04-03 11:44:10 -0400

    parses censustracts2010.kml

---
+++ fetchcalls/census.py
@@ -0,0 +1,35 @@
+from lxml import etree
+import shapely
+import re
+tree = etree.parse(open("censustracts2010.kml","r"))
+
+from shapely.geometry import Point, Polygon
+def topoint(s):
+    return (float(s[0]),float(s[1]))
+
+
+context = etree.iterparse(open("censustracts2010.kml","r"))
+id = ''
+tracts = {}
+tracts_to_area = {}
+
+area_re = r'Chicago Community Area</td>\r\r\n<td>([0-9]+)'
+tract_re = r'Census Tract ([0-9.]+)'
+#print "id,area,tract"
+
+
+
+for action, e in context:
+    if "Placemark" in e.tag:
+        id = e.values()[0][3:]
+        coords = e.iterdescendants(tag='{http://www.opengis.net/kml/2.2}coordinates').next().text
+	html = e.iterdescendants(tag='{http://www.opengis.net/kml/2.2}description').next().text
+	match = re.search(area_re, html)
+	area = match.group(1)
+	match = re.search(tract_re, html)
+	tract = match.group(1)
+	#print "%s,%s,%s"%(id, area, tract)
+	
+        tracts[id] = Polygon([topoint(c.split(',')) for c in coords.split()[1:]])
+	tracts_to_area[id] = [tract,area]
+
