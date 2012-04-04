cd923f09be1cb506e79eba354e81daf2ec5c238b
Xin Wu <nova2358@gmail.com>
2012-04-04 11:07:40 -0400

    fixed filename

--- fetchcalls/311.sh
+++ fetchcalls/311.sh
@@ -4,6 +4,6 @@
 python fetch.py 311.csv
 
 # geocode 311.csv
-python geocode_311_data.py 311.csv > 311-geocoded.csv
+python geocode.py 311.csv > 311-geocoded.csv
 python postprocess.py 311-geocoded.csv > 311-census-violent.csv
 
