4f1188772e5292bc79e83db14a7c3b10d2c0f3d3
Seth Flaxmaxn <flaxman@cmu.edu>
2012-07-05 13:51:07 -0400

    ready to share, but bugs remain

--- example-streams.py
+++ example-streams.py
@@ -1,4 +1,4 @@
-#### This code searches over subsets of streams aggregated over a fixed set of locations
+#### This code searches for an optimal subset of streams, aggregated over a fixed set of locations
 
 #### Ideas for exploratory data analysis: 
 #### 1. Try different sets of locations (very small vs. very large)
@@ -6,38 +6,36 @@
 ####    the training data set and see whether any of the correlations you find are still significant
 ####    for the test data set.
 
-RADIUS = .01
-
 from ccss import *
-from ccss import greedy_search as search
-
-print "precalculating centers of all tracts (this may take a little while)"
-with mytimer:
-    tract_centers = calculate_tract_centers(input)
-
-print "completed in %d seconds"%mytimer.elapsed
+from ccss import greedy_streams as search_streams
 
 f = open(opts.output,"w")
 out = csv.writer(f)
-out.writerow("predict tract R Dopt Sopt Xn Yn elapsed".split())
+out.writerow("predict R Dopt S Xn Yn elapsed".split())
 
+matched = "all"
 
-n_tracts = len(np.unique(input['tract'])) * 1.0
-for i, tract in enumerate(np.unique(input['tract'])):
-    data = input[match_tracts(input, nearby_tracts(tract_centers[tract],input,RADIUS))]
-    with mytimer:
-        R, Sopt, Dopt, iters = search(data, np.array(streams))
+if opts.areas != "all":
+    print "Matching areas", opts.areas
+    data = input[match_areas(input, opts.areas)]
+    matched = opts.areas
+elif opts.tracts != "all":
+    print "Matching tracts", opts.tracts
+    data = input[match_tracts(input, opts.tracts)]
+    matched = opts.tracts
+else:
+    data = input
 
-    Xn = np.sum(match_streams(data,Dopt) * match_tracts(data,Sopt))
-    Yn = np.sum(match_streams(data,[opts.predict]) * match_tracts(data,Sopt))
-    print "\n\n-------------------- TRACT", tract
-    print "Correlation = %.05f"%R
-    print "for predicting", opts.predict, "with these leading indicators:", ' '.join(Dopt)
-    print "# of events in X:", Xn
-    print "# of events in Y:", Yn
-    if np.abs(R) > .3:
-        out.writerow([opts.predict, tract, R, Dopt, Sopt, Xn, Yn, mytimer.elapsed])
-    f.flush()
-    print "%d %% done", i / n_tracts
+with mytimer:
+    R, Dopt = search_streams(data, np.array(streams))
+
+Xn = np.sum(match_streams(data,Dopt))
+Yn = np.sum(match_streams(data,[opts.predict]))
+print "\n\n-------------------- In area", matched
+print "Correlation = %.05f"%R
+print "for predicting", opts.predict, "with these leading indicators:", ', '.join(Dopt)
+print "# of events in X:", Xn
+print "# of events in Y:", Yn
+out.writerow([opts.predict, R, Dopt, matched, Xn, Yn, mytimer.elapsed])
 
 print "search complete, output written to %s"%(opts.output)
