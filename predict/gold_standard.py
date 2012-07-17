0859b7d1d3f8f75892d7cbb914ba3650ab054c02
Seth Flaxmaxn <flaxman@cmu.edu>
2012-07-17 13:55:49 -0400

    first commit

--- predict/gold_standard.py
+++ predict/gold_standard.py
@@ -1,25 +1,53 @@
 import sqlite3 as sql
+import csv
 from pandas import *
 import utils
 from dateutil import parser
 from datetime import timedelta as td
 import datetime
+from optparse import OptionParser
+import csv
+
+
+THETA_THRESH = 2
+C_THRESH = 5
+AREAL = "beat"
+
+outf = "out.csv"
+if __name__  == "__main__":
+    op = OptionParser()
+    op.add_option("--theta",dest="theta",help="threshold for theta (default is 2)", default=2, type=float)
+    op.add_option("-c", dest="c",help="threshold for counts c (default is 5)", default=5, type=float)
+    op.add_option("--areal", dest="areal",help="areal unit to use (default is beat)", default="beat")
+    op.add_option("--output", dest="output",help="output file use (default is out.csv)", default="out.csv")
+    op.add_option("--stream", dest="stream",help="stream type to predict (default is VIOLENT)", default="stream")
+    opts = op.parse_args()[0]
+
+    outf = opts.output
+    THETA_THRESH = opts.theta
+    C_THRESH = opts.c
+    AREAL = opts.areal
 
 db = utils.load("input.db")
 
 # get a list of areal units
-areals = utils.query(db,"SELECT DISTINCT beat FROM data")
+areals = utils.query(db,"SELECT DISTINCT %s FROM data"%AREAL)
+if u'NA' in areals:
+    areals.remove(u'NA')
+
 n_areals = len(areals)
 print "Number of areal units:", n_areals
 
 #dates = utils.query(db,"SELECT DISTINCT date FROM data WHERE date between '2011-01-01' and '2012-06-30'")
 #dates = [parser.parse(d).date() for d in dates]
 
-
-vc = read_csv("violent-by-beat.csv",converters={'date':lambda d: parser.parse(d).date()})
+#vc = read_csv("violent-by-beat.csv",converters={'date':lambda d: parser.parse(d).date()})
+#vc = read_csv("geocoded.csv",converters={'date':lambda d: parser.parse(d).date()}, names=("date","beat","type"))
+vc = csv.reader(open("geocoded.csv","r"))
+vc.next()
 
 # get a list of dates
-start_date = min(vc.date)
+start_date = datetime.date(2011, 1, 1) #min(vc.date)
 end_date = datetime.date(2012, 4, 30) #max(vc.date)
 print "Date range:", start_date, "-", end_date
 
@@ -28,8 +56,10 @@ Braw = {}
 Craw = {}
 Coverall = np.zeros(n_dates)
 
-for date,beat in vc.values:
-    if date <= end_date and date >= start_date: 
+for date,stream,X,Y,beat,tract,area in vc:
+    date = parser.parse(date).date()
+    
+    if date <= end_date and date >= start_date and beat != 'NA' and stream == opts.stream:
         t0 = (date - start_date).days
         if not beat in Craw:
             Craw[beat] = np.zeros(n_dates)
@@ -49,23 +79,34 @@ gold = {}
 theta = {}
 
 rolling_total = rolling_sum(Coverall,7)[6:]
-chk = 0.0
-THETA_THRESH = 2
-C_THRESH = 5
 n_on = {}
+n_on_tot = 0
 
 for beat in Craw.keys():
     C[beat] = rolling_sum(Craw[beat],7)[6:]
 
     B[beat] = (total_by_beat[beat] / Ctotal) * rolling_total
-    chk += total_by_beat[beat] / Ctotal
     theta[beat] = (C[beat] - B[beat]) / np.sqrt(B[beat])
 
     gold[beat] = (theta[beat] > THETA_THRESH) * (C[beat] > C_THRESH)
     n_on[beat] = sum(gold[beat])
+    n_on_tot += sum(gold[beat])
 
+#import pylab as pl
+#pl.plot(range(len(gold[beat])),Craw[beat][6:])
+#pl.plot(range(len(gold[beat])),gold[beat]*8, '.', markersize=25)
+#pl.show()
+print "Total number of areal units that are on over time", n_on_tot
 
-pl.plot(range(len(gold[beat])),Craw[beat][6:])
-pl.plot(range(len(gold[beat])),gold[beat]*8, '.', markersize=25)
-pl.show()
+fo = open(outf,"w")
+out = csv.writer(fo)
+out.writerow([AREAL, 'date', 'on'])
 
+start_date = start_date + td(6)
+n_dates = (end_date - start_date).days + 1
+
+for beat in gold.keys():
+    for i in range(n_dates):
+        out.writerow([int(beat), start_date + td(i), int(gold[beat][i])])
+
+fo.flush()
